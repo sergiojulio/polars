0889256d68403406e695e05f6a3e6873f2c85a4f
sergiojulio <sergiojulio@gmail.com>
2023-03-12 09:31:51 -0300

    Polars hello world

---
+++ main.py
@@ -0,0 +1,14 @@
+import polars as pl
+
+df = pl.DataFrame(
+     {
+         'Model': ['iPhone X','iPhone XS','iPhone 12',
+                   'iPhone 13','Samsung S11','Samsung S12',
+                   'Mi A1','Mi A2'],
+         'Sales': [80,170,130,205,400,30,14,8],     
+         'Company': ['Apple','Apple','Apple','Apple',
+                     'Samsung','Samsung','Xiao Mi','Xiao Mi'],
+     }
+)
+
+print(df)
